5bbd2e067d5c0d3065f4ea971b0ad2b997941c7c
venigalla2410 <gsaivenigalla@gmail.com>
2020-02-07 11:42:54 +0530

    message

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class swap {
+	pubic static void main (String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a value is :"+a+" "+"b value is :"+b);
+	}
+}
